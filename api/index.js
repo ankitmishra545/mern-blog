ca48b1542747b1476e66ba3e912d8464c111aaa5
Ankit <ankitmishra545@gmail.com>
2024-01-18 10:33:39 +0530

    added middleware and a errorHandler function to handle custom errors

--- api/index.js
+++ api/index.js
@@ -22,3 +22,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    console.log("middleware==>", err.statusCode, err.message)
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+}) 
