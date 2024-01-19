f26efc70141cab195e320a6183e6e6256dd0dac0
Ankit <ankitmishra545@gmail.com>
2024-01-19 17:54:39 +0530

    dashboard sidebar added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 const Dashboard = () => {
+
+  const location = useLocation();
+
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search])
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <div>
+        <DashSidebar/>
+      </div>
+      {tab === 'profile' && <DashProfile/>}
+    </div>
   )
 }
 
-export default Dashboard
+export default Dashboard 
