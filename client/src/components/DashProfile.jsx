012ffb91ac08a86b914068270ec2330dcc9308c0
Ankit Mishra <ankitmishra545@gmail.com>
2024-01-22 16:49:40 +0530

    completed image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,17 +1,84 @@
 import { useSelector } from 'react-redux';
-import { Button, TextInput } from 'flowbite-react'
+import { Alert, Button, TextInput } from 'flowbite-react'
+import { useEffect, useRef, useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const DashProfile = () => {
 
   const {currentUser} = useSelector(state => state.user);
 
+  const filePickerRef = useRef();
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if(file){
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const imageUploadFile = async() => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError('Could not upload image (file must be less than 2MB)');
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadUrl => {
+          setImageFileUrl(downloadUrl);
+        })
+      }
+    )
+  };
+
+  useEffect(() => {
+    if(imageFile){
+      imageUploadFile();
+    }
+  }, [imageFile])
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 font-semibold text-center text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-          <img src={currentUser.profilePicture} alt='profile-pic' className='w-full h-full rounded-full border-8 border-[lightgray] object-cover'/>
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={() => filePickerRef.current.click()}>
+          {imageFileUploadProgress && <CircularProgressbar value={imageFileUploadProgress || 0} text={`${imageFileUploadProgress}%`} strokeWidth={5} styles={{
+            root: {
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+              top: 0,
+              left: 0
+            },
+            path: {
+              stroke: `rgba(62, 152, 199, ${imageFileUploadProgress / 100})`
+            }
+          }} />}
+          <img src={imageFileUrl || currentUser.profilePicture} alt='profile-pic' className={`w-full h-full rounded-full border-8 border-[lightgray] object-cover ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'}`}/>
         </div>
+        {imageFileUploadError && (
+          <Alert color='fialure'>{imageFileUploadError}</Alert>
+        )}
         <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username}/>
         <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email}/>
         <TextInput type='password' id='password' placeholder='Password' defaultValue=''/>
