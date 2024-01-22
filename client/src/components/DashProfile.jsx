cb68dc3408e86f4a7a9c03ef0b1282615ec62336
Ankit Mishra <ankitmishra545@gmail.com>
2024-01-22 14:34:55 +0530

    completed profile page ui and some css classes to sidebar

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { Button, TextInput } from 'flowbite-react'
 
 const DashProfile = () => {
+
+  const {currentUser} = useSelector(state => state.user);
+
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 font-semibold text-center text-3xl'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+          <img src={currentUser.profilePicture} alt='profile-pic' className='w-full h-full rounded-full border-8 border-[lightgray] object-cover'/>
+        </div>
+        <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username}/>
+        <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email}/>
+        <TextInput type='password' id='password' placeholder='Password' defaultValue=''/>
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
+      </form>
+      <div className='flex justify-between mt-5 text-red-500'>
+        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -18,8 +18,8 @@ const Dashboard = () => {
   }, [location.search])
 
   return (
-    <div>
-      <div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
         <DashSidebar/>
       </div>
       {tab === 'profile' && <DashProfile/>}
