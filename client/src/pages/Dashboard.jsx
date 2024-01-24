4b968bdfba15b9bc6481ec3528580d295049f6df
Ankit <ankitmishra545@gmail.com>
2024-01-24 10:40:09 +0530

    dashpost component added to dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 const Dashboard = () => {
 
@@ -20,9 +21,13 @@ const Dashboard = () => {
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>
+        {/* sidebar */}
         <DashSidebar/>
       </div>
+        {/* profile */}
       {tab === 'profile' && <DashProfile/>}
+        {/* posts */}
+      {tab === 'posts' && <DashPosts/>}
     </div>
   )
 }
